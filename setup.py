b1ca9a795c1a639161ec4655a129bcf1a51c3ba8
cesargodoi <cesar.godoi@gmail.com>
2020-07-24 21:31:39 -0300

    Preparing to PyPI

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
 
-def read(filename):
-    return [req.strip() for req in open(filename).readlines()]
-
-
 setup(
     name="start_flask",
     version="0.1.0",
-    description="Start a Flask project in a Application Factory Pattern",
     author="Cesar Godoi",
     author_email="cesar.godoi@gmail.com",
     packages=["start_flask"],
     package_dir={"start_flask": "start_flask"},
-    entry_points={"console_scripts": ["start_flask=start_flask.cli:main"]},
     include_package_data=True,
+    description="Start a Flask project in a Application Factory Pattern",
+    url="https://github.com/cesargodoi/start_flask",
+    project_urls={"source code": "https://github.com/cesargodoi/start_flask",},
+    license="GNU GPLv3",
+    entry_points={"console_scripts": ["start_flask=start_flask.cli:main"]},
     install_requires=["jinja2", "Click"],
 )
