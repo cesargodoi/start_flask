e22ce2600e3572e55eb7ff8f3e9b12ede34f003f
cesargodoi <cesar.godoi@gmail.com>
2020-07-31 09:00:02 -0300

    version 0.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="start_flask",
-    version="0.1.5",
+    version="0.1.6",
     author="Cesar Godoi",
     author_email="cesar.godoi@gmail.com",
     packages=["start_flask"],
