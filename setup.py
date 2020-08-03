9891e2178d57f27efde9378772f2111dc3b68a0a
cesargodoi <cesar.godoi@gmail.com>
2020-08-03 16:43:32 -0300

    Adjust MANIFEST.in

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="start_flask",
-    version="0.1.7",
+    version="0.1.8",
     author="Cesar Godoi",
     author_email="cesar.godoi@gmail.com",
     packages=["start_flask"],
